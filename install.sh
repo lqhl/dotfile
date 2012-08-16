c9a60e874e9cfaeccb0941fab53007a42e617a19
Qin Liu <lqgy2001@gmail.com>
2012-08-16 15:49:36 +0800

    modification

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 cd ~/.dotfile
 git submodule init
 git submodule update
+
 ln -s ~/.dotfile/.vim_runtime ~
 sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+ln -s ~/.dotfile/.gitglobalignore ~
+git config --global core.excludesfile ~/.gitglobalignore
+
